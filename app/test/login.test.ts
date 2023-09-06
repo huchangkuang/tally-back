485a076bd94dbceb22250a4e89517ed2fe43c72a
huchangying <603021137@qq.com>
2023-09-06 18:15:36 +0800

    add jest to test request

--- app/test/login.test.ts
+++ app/test/login.test.ts
@@ -1,5 +1,5 @@
 import run from "../server/app";
-import supertest from "supertest";
+import request from "supertest";
 import { Server } from "http";
 
 describe("login", () => {
@@ -8,7 +8,15 @@ describe("login", () => {
     app = run(3456);
   });
   it("get /login", function () {
-    supertest(app).get("/login").expect(200);
+    request(app).get("/login").expect(200);
+  });
+  it("post /login/signUp", function () {
+    request(app)
+      .post("/login/signUp")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data).toEqual("login1");
+      });
   });
   afterAll(() => {
     app.close();
