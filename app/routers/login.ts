807b67397563326d7dd67d32b6779559d032499e
huchangying <603021137@qq.com>
2023-09-06 14:05:49 +0800

    commit auto prettier

--- app/routers/login.ts
+++ app/routers/login.ts
@@ -2,15 +2,17 @@ import Router from "koa-router";
 import { Context } from "koa";
 
 const routers = new Router();
-routers.get("/", (ctx: Context) => {
-  ctx.body = 'hello';
-  ctx.res.statusCode = 200;
-}).post("/signUp", (ctx: Context) => {
-  ctx.body = {
-    msg: "success",
-    data: "login",
-  };
-  ctx.res.statusCode = 200;
-});
+routers
+  .get("/", (ctx: Context) => {
+    ctx.body = "hello";
+    ctx.res.statusCode = 200;
+  })
+  .post("/signUp", (ctx: Context) => {
+    ctx.body = {
+      msg: "success",
+      data: "login",
+    };
+    ctx.res.statusCode = 200;
+  });
 
 export default routers;
