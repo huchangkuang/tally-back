c45512a704862da63e38874564629587a718769f
huchangying <603021137@qq.com>
2023-09-08 17:59:23 +0800

    modify login test

--- app/test/login.test.ts
+++ app/test/login.test.ts
@@ -3,23 +3,27 @@ import request from "supertest";
 import { Server } from "http";
 import config from "../../config";
 
+// jest.useFakeTimers()
+
 describe("login", () => {
   let app: Server;
-  beforeAll(() => {
+  beforeAll((done) => {
     app = run(config.serverPort);
+    done();
   });
-  it("get /login", function () {
-    request(app).get("/login").expect(200);
+  it("get /login", async () => {
+    await request(app).get("/login").expect(200);
   });
-  it("post /login/signUp", function () {
-    request(app)
+  it("post /login/signUp", async () => {
+    await request(app)
       .post("/login/signUp")
       .expect(200)
       .expect((res) => {
-        expect(res.body.data).toEqual("login1");
+        expect(res.body.data).toEqual("login");
       });
   });
-  afterAll(() => {
+  afterAll((done) => {
     app.close();
+    done();
   });
 });
