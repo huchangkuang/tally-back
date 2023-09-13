90051afcae4c20f5d78e1dd7034a8eb3eb5566e9
huchangying <603021137@qq.com>
2023-09-13 17:30:06 +0800

    封装路由登录权限拦截

--- app/utils/jwtValidate.ts
+++ app/utils/jwtValidate.ts
@@ -1,10 +1,24 @@
 import jwt from "jsonwebtoken";
 
-export const sign = (option: { idName: string; password: string }) =>
+export const getToken = (header) => {
+  const { authorization } = header;
+  return authorization?.startsWith("Bearer ") ? authorization.slice(7) : "";
+};
+
+export const jwtSign = (option: { idName: string; password: string }) =>
   jwt.sign(
     { ...option, iat: Math.floor(Date.now() / 1000) },
     process.env.PRIVATE_KEY,
     { expiresIn: "7d" },
   );
 
-export const verify = (token) => jwt.verify(token, process.env.PRIVATE_KEY);
+export const jwtVerify = (token: string) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.PRIVATE_KEY, (error, decode) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(decode);
+      }
+    });
+  });
