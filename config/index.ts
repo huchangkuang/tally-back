22ed1ae30e4eedad9ed6f9cbeef7dd6c5368ea20
huchangying <603021137@qq.com>
2023-10-07 16:10:55 +0800

    docker-compose

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,8 @@
 const config = {
-  serverPort: 3456,
+  serverPort: 39002,
   db: {
     database: "test_db",
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     username: "root",
     password: process.env.DB_PASSWORD,
   },
